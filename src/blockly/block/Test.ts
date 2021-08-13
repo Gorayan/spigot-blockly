cc44751094047ab5b9b246a6e7da859405786583
Gorayan <Gorakun24@gmail.com>
2021-08-14 07:03:26 +0900

    blocklyテスト用

---
+++ src/blockly/block/Test.ts
@@ -0,0 +1,48 @@
+import Blockly from 'blockly';
+
+Blockly.defineBlocksWithJsonArray([
+    {
+        "type": "test",
+        "lastDummyAlign0": "RIGHT",
+        "message0": "a %1 b",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "NAME",
+                "check": "Player"
+            }
+        ],
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "event_player",
+        "message0": "参加したプレイヤー",
+        "output": "Player",
+        "colour": 100,
+        "tooltip": "",
+        "helpUrl": ""
+    },
+    {
+        "type": "sendmessage",
+        "message0": "%1 に %2 とメッセージを送る",
+        "args0": [
+            {
+                "type": "input_value",
+                "name": "player",
+                "check": "Player"
+            },
+            {
+                "type": "input_value",
+                "name": "text",
+                "check": "String"
+            }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 230,
+        "tooltip": "",
+        "helpUrl": ""
+    }
+]);
