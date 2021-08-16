1ed3ae048c0dc94d4eaf694b7dfa8970cd0ab308
Gorayan <Gorakun24@gmail.com>
2021-08-16 09:39:06 +0900

    reduxでstate管理

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,17 @@
 import {configureStore} from "@reduxjs/toolkit";
-import tabReducer from "./slice";
+import tabReducer from "./tab/slice";
+import workspaceReducer from "./workspace/slice";
 
 export const store = configureStore({
     reducer: {
         tab: tabReducer,
+        workspace: workspaceReducer
     },
+    middleware: (getDefaultMiddleware => {
+        return getDefaultMiddleware({
+            serializableCheck: false,
+        })
+    })
 })
 
 export type AppDispatch = typeof store.dispatch;
---
+++ src/redux/workspace/slice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit'
+import {spigotGenerator} from "../../blockly/generator/generator";
+import {WorkspaceSvg} from "blockly";
+
+interface state {
+    workspace?: WorkspaceSvg,
+    code: string
+}
+
+const initialState: state = {
+    code: ""
+}
+
+export const workspaceSlice = createSlice({
+    name: 'workspace',
+    initialState,
+    reducers: {
+        generate: (state) => {
+            state.code = spigotGenerator.workspaceToCode(state.workspace);
+        },
+        setWorkspace: (state, action) => {
+            state.workspace = action.payload;
+        }
+    },
+})
+
+// Action creators are generated for each case reducer function
+export const { generate, setWorkspace} = workspaceSlice.actions
+
+export default workspaceSlice.reducer
