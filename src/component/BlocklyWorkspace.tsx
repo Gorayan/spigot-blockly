6e2579942a70816456379d43a6320d29fac66903
Gorayan <Gorakun24@gmail.com>
2021-08-21 11:37:15 +0900

    workspaceの更新をlistenerで

--- src/component/BlocklyWorkspace.tsx
+++ src/component/BlocklyWorkspace.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import toolboxJson from "../resource/test.json"
 import Blockly, {WorkspaceSvg} from "blockly";
 import {useDispatch, useSelector} from "react-redux";
@@ -29,8 +29,6 @@ function BlocklyWorkspace(props: Props) {
 
     const dispatch = useDispatch<AppDispatch>();
 
-    const [workspace, setWorkspace] = useState<WorkspaceSvg>()
-
     useEffect(() => {
 
         if (blocklyDivRef.current === null) {
@@ -39,25 +37,18 @@ function BlocklyWorkspace(props: Props) {
 
         blocklyDivRef.current.innerHTML = ""
 
-        const newWorkspace = Blockly.inject(blocklyDivRef.current, {
+        const workspace: WorkspaceSvg = Blockly.inject(blocklyDivRef.current, {
             toolbox: toolboxJson as Blockly.utils.toolbox.ToolboxDefinition
         })
 
-        setWorkspace(newWorkspace)
-
+        workspace.addChangeListener(() => {
+            const dom = Blockly.Xml.workspaceToDom(workspace)
+            const workspace_text = Blockly.Xml.domToText(dom)
+            dispatch(setHandleFile({...file, workspace: workspace_text}))
+            // TODO generator も追加する
+        })
 
     }, [file.id])
-
-    useEffect(() => {
-        if (workspace === undefined) {
-            return
-        }
-        console.log("file update")
-        const dom = Blockly.Xml.workspaceToDom(workspace)
-        const workspace_text = Blockly.Xml.domToText(dom)
-        dispatch(setHandleFile({...file, workspace: workspace_text}))
-        // TODO generator も追加する
-    }, [workspace])
 
     return (
         <div ref={blocklyDivRef} className={classes.blockDiv} hidden={props.hidden} />
