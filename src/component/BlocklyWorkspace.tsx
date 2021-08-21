b14b318b3020f544a1eb1b24b8301e709515a289
Gorayan <Gorakun24@gmail.com>
2021-08-21 16:37:01 +0900

    import optimize

--- src/component/BlocklyWorkspace.tsx
+++ src/component/BlocklyWorkspace.tsx
@@ -3,7 +3,7 @@ import toolboxJson from "../resource/test.json"
 import Blockly, {WorkspaceSvg} from "blockly";
 import {useDispatch, useSelector} from "react-redux";
 import {AppDispatch, RootState} from "../redux/store";
-import {FileWorkspace, setHandleFile, setCode} from "../redux/workspace/slice";
+import {FileWorkspace, setCode, setHandleFile} from "../redux/workspace/slice";
 import {createStyles, makeStyles} from "@material-ui/core/styles";
 import {spigotGenerator} from "../blockly/generator/generator";
 
