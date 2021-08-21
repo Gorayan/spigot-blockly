4a06e1eb5fac8e55364be2355158460cdd98e472
Gorayan <Gorakun24@gmail.com>
2021-08-21 10:06:49 +0900

    opening_fileをFileWorspace型に変更 localstorageの読み込みはAppで(Router変更時に読み込まれない) reducerの型を定義

--- src/redux/workspace/slice.ts
+++ src/redux/workspace/slice.ts
@@ -1,4 +1,4 @@
-import {createSlice} from '@reduxjs/toolkit'
+import {createSlice, PayloadAction} from '@reduxjs/toolkit'
 import {spigotGenerator} from "../../blockly/generator/generator";
 import Blockly, {WorkspaceSvg} from "blockly";
 
@@ -13,43 +13,41 @@ export interface workspaceState {
     workspace?: WorkspaceSvg,
     code: string
     selected_file: string
-    opening_file?: string
+    handle_file: FileWorkspace
     files: FileWorkspace[]
 }
 
 const initialState: workspaceState = {
     selected_file: "",
     code: "",
-    files: readLocalStorage()
-}
-
-function readLocalStorage() {
-    const rawData = localStorage.getItem("files")
-    let files: FileWorkspace[] = []
-    if (rawData !== null) {
-        files = JSON.parse(rawData) as FileWorkspace[]
-    }
-    return files
+    handle_file: createFile(),
+    files: []
 }
 
 export const workspaceSlice = createSlice({
     name: 'workspace',
     initialState,
     reducers: {
+        reloadHanldeFile: (state) => {
+            if (state.workspace === undefined) {
+                return
+            }
+            state.handle_file = {...state.handle_file, workspace: Blockly.Xml.domToText(state.workspace.createDom())}
+        },
         generate: (state) => {
             state.code = spigotGenerator.workspaceToCode(state.workspace);
         },
         setWorkspace: (state, action) => {
             state.workspace = action.payload;
         },
-        saveFile: (state, action) => {
+        saveFile: (state, action: PayloadAction<string>) => {
             const workspace = state.workspace
             if (workspace === undefined) {
                 return
             }
             const dom = workspace.createDom()
             const workspace_text = Blockly.Xml.domToText(dom)
-            const id = state.opening_file === undefined ? getUniqueStr() : state.opening_file
+            const id = state.handle_file === undefined ? getUniqueStr() : state.handle_file.id
             const date = new Date()
             const file: FileWorkspace = {
                 id: id,
@@ -58,22 +56,34 @@ export const workspaceSlice = createSlice({
                 workspace: workspace_text
             }
             state.files = [...state.files.filter((file) => file.id !== id), file]
-            state.opening_file = id
+            state.handle_file = file
         },
-        deleteFile: (state, action) => {
+        deleteFile: (state, action: PayloadAction<string>) => {
             state.files = state.files.filter((file) => file.id !== action.payload)
         },
-        selectFile: (state, action) => {
+        selectFile: (state, action: PayloadAction<string>) => {
             state.selected_file = action.payload
         },
-        setOpeningFile: (state, action) => {
-            state.opening_file = action.payload
+        setHandleFile: (state, action: PayloadAction<FileWorkspace>) => {
+            state.handle_file = action.payload
         },
-        setFiles: (state, action) => {
+        setFiles: (state, action: PayloadAction<FileWorkspace[]>) => {
             state.files = action.payload
-        }
+        },
     },
 })
+
+function createFile() {
+    const id = getUniqueStr()
+    const date = new Date()
+    const file: FileWorkspace = {
+        id: id,
+        name: "",
+        date: (date.getFullYear()) + "/" + toDoubleDigits(date.getMonth() + 1) + "/" + toDoubleDigits(date.getDate()) + " " + toDoubleDigits(date.getHours()) + ":" + toDoubleDigits(date.getMinutes()),
+        workspace: ""
+    }
+    return file
+}
 
 function getUniqueStr(myStrong?: number): string {
     let strong = 1000;
@@ -90,9 +100,9 @@ function toDoubleDigits(num: any) {
         num = "0" + num;
     }
     return num;
-};
+}
 
 // Action creators are generated for each case reducer function
-export const { generate, setWorkspace, saveFile, deleteFile, selectFile, setOpeningFile, setFiles } = workspaceSlice.actions
+export const { generate, setWorkspace, saveFile, deleteFile, selectFile, setHandleFile, setFiles } = workspaceSlice.actions
 
 export default workspaceSlice.reducer
